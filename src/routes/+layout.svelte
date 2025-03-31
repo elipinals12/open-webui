976a00621b3173b81259a626285bb5695073b334
Eli <elipinals12@gmail.com>
2025-03-31 15:04:05 -0400

    adj. disclaimer formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -621,9 +621,9 @@
 		class="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black/50 dark:bg-black/60 z-50"
 	>
 		<div
-			class="p-6 w-11/12 max-w-screen-xl bg-gray-800 dark:bg-gray-900 text-center text-white dark:text-gray-100 overflow-auto max-h-[85vh]"
+			class="p-2 w-11/12 max-w-screen-2xl bg-gray-800 dark:bg-gray-900 text-center text-white dark:text-gray-100 overflow-auto max-h-[90vh]"
 		>
-			<div class="px-4">
+			<div class="p-4">
 				<img
 					src="/assets/images/HumanSubjectResearchProtection.png"
 					alt="Northeastern University - Human Subject Research Protection"
